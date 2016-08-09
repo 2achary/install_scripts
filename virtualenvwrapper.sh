8a9bc56d29990d0b1e4db835a9f58c344b2bb7f4
Zach Swift <cras.zswift@gmail.com>
2016-08-09 10:48:37 -0500

    centos

--- virtualenvwrapper.sh
+++ virtualenvwrapper.sh
@@ -2,3 +2,12 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
 source /usr/local/bin/virtualenvwrapper.sh
 sudo pip install --ignore-installed six virtualenvwrapper
+
+# for centos
+pip3 install virtualenvwrapper
+export WORKON_HOME=~/Envs
+mkdir -p $WORKON_HOME
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.4
+source /usr/bin/virtualenvwrapper.sh
+source ~/.bashrc
+mkvirtualenv myvenv
